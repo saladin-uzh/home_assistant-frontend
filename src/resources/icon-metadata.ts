8217152162a167e10f0ad1c23ebdda415c89181c
Sam V. <openminds.dev.studio@gmail.com>
2026-01-26 16:26:43 +0200

    Minor fix

--- src/resources/icon-metadata.ts
+++ src/resources/icon-metadata.ts
@@ -1,4 +1,4 @@
-import * as iconMetadata_ from "../../build/mdi/iconMetadata.json";
+import * as iconMetadata_ from "../../build/mdi/iconList.json";
 import type { IconMetaFile } from "../types.js";
 
 export const iconMetadata = (iconMetadata_ as any).default as IconMetaFile;
